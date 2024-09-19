3d79de6ed97a72c1d6ab4c4dd767fb78ab98589b
zach <zdefranc@uwaterloo.ca>
2024-09-19 18:31:44 -0400

    Standerdized the dimensions of the robot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,8 @@ import { useState, useEffect, useCallback } from "react"
 function App() {
   const [count, setCount] = useState<number | null>(null)
 
-  //Calls useEffect when the specified value changes
-  // useEffect(() => {
-  //   console.log('mounting')
-  //   console.log("Users: ", users)
-
-  //   return () => console.log("unmounting")
-  // }, [users])
-
-  // Use for expensive computations to keep from recompiling/rerendering function
-  const addTwo = useCallback(() => setCount(prev => prev!=null?prev+2:0), [count])
-
   return (
     <div>
-      <h1>Control Robot</h1>
       <Robot />
     </div>
   );
--- frontend/src/components/RobotVisualization.tsx
+++ frontend/src/components/RobotVisualization.tsx
@@ -1,23 +1,38 @@
 import * as THREE from 'three';
 import { RobotState } from '../types/RobotTypes';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 import { useEffect, useRef } from "react";
 
-const BASE_HEIGHT = 0.3;
+const BASE_HEIGHT = 0.3; // Important for IK
+const BASE_RADIUS = 0.8; 
 
-const LIFT_HEIGHT = 4;
+const LIFT_HEIGHT = 4; 
+const LIFT_RADIUS = 0.3;
 
-const ELBOW_LENGTH = 2;
+const ELBOW_LENGTH = 2; // Important for IK
+const ELBOW_WIDTH = 0.3;
+const ELBOW_DEPTH = 0.5;
 
-const WRIST_LENGTH = 1;
+const WRIST_LENGTH = 1; // Important for IK
+const WRIST_WIDTH = 0.3;
+const WRIST_DEPTH = 0.4;
 
-const GRIPPER_BASE_LENGTH = 0.4;
+const WRIST_STEM_LENGTH = WRIST_WIDTH+ELBOW_WIDTH;
+const WRIST_STEM_RADIUS = 0.2;
 
-const GRIPPER_BASE_WIDTH = 0.4;
+const GRIPPER_BASE_LENGTH = 0.6;
+const GRIPPER_BASE_WIDTH = 0.1;
+const GRIPPER_BASE_DEPTH = 0.4;
 
-const GRIPPERS_WIDTH = 0.05;
+const GRIPPER_STEM_LENGTH = 0.7;
+const GRIPPER_STEM_RADIUS = 0.15;
 
-const GRIPPERS_LENGTH = 0.2;
+const GRIPPERS_HEIGHT = 0.05;
+const GRIPPERS_WIDTH = 0.4;
+const GRIPPERS_LENGTH = GRIPPER_BASE_WIDTH+0.2;
+
 
 type RobotVisualizationProps = {
   robotState: RobotState | null;
@@ -40,14 +55,15 @@ function RobotVisualization(props: RobotVisualizationProps) {
     scene.background = new THREE.Color(0xeeeeee);
 
     // Create a camera
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth/2 / window.innerHeight, 0.1, 1000);
     camera.position.z = 10;
-    camera.position.y = 2;
+    camera.position.y = 3;
     camera.position.x = 3;
 
     const gridHelper = new THREE.GridHelper(100, 100);
     gridHelper.position.y = 0;
     scene.add(gridHelper);
+
     
     // Create the renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -57,6 +73,9 @@ function RobotVisualization(props: RobotVisualizationProps) {
     // use ref as a mount point of the Three.js scene instead of the document.body
     // This keeps multiple simulations from appearing.
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
+
+    // Provides an orbital camera.
+    const controls = new OrbitControls(camera, renderer.domElement);
     
     // Add light to the scene
     const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -67,71 +86,76 @@ function RobotVisualization(props: RobotVisualizationProps) {
     const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
 
     // Base
-    const base = new THREE.Mesh(new THREE.CylinderGeometry(0.8, 0.8, BASE_HEIGHT, 32), material);
+    const base = new THREE.Mesh(new THREE.CylinderGeometry(BASE_RADIUS, BASE_RADIUS, BASE_HEIGHT, 32), material);
     base.position.set(0, BASE_HEIGHT/2, 0);
     scene.add(base);
     
-    // swing joint
+    // swing joint (DOF 1)
     const swingJoint = new THREE.Object3D();
     swingJoint.position.set(0, BASE_HEIGHT/2, 0)
     base.add(swingJoint);
 
-    // Not the should name it different. Shoulder (DOF 2)
-    const lift = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, LIFT_HEIGHT, 32), material);
+    // Lift. 
+    const lift = new THREE.Mesh(new THREE.CylinderGeometry(LIFT_RADIUS, LIFT_RADIUS, LIFT_HEIGHT, 32), material);
     lift.position.set(0, LIFT_HEIGHT/2, 0);
     swingJoint.add(lift);
 
-    // Linear lift joint
+    // Linear lift joint (DOF 2)
     const liftJoint = new THREE.Object3D();
-    liftJoint.position.set(0, 0, 0)
+    // Come back to this so the arm isnt in the floor at zero!
+    liftJoint.position.set(0, -LIFT_HEIGHT/2, 0)
     lift.add(liftJoint);
 
-    // Elbow (DOF 3)
-    const elbow = new THREE.Mesh(new THREE.BoxGeometry(0.5, ELBOW_LENGTH, 0.5), material);
+    // Elbow 
+    const elbow = new THREE.Mesh(new THREE.BoxGeometry(ELBOW_WIDTH, ELBOW_LENGTH, ELBOW_DEPTH), material);
     elbow.position.set(ELBOW_LENGTH/2, 0, 0);
     elbow.rotation.set(0, 0, Math.PI/2);
     liftJoint.add(elbow);
 
+    // Elbow joint (DOF 3)
     const elbowJoint = new THREE.Object3D();
     elbowJoint.position.set(0, -ELBOW_LENGTH/2, 0)
     elbow.add(elbowJoint);
 
-    const wristStem = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 0.9, 32), material);
-    wristStem.position.set(-0.2, 0, 0);
+    const wristStem = new THREE.Mesh(new THREE.CylinderGeometry(WRIST_STEM_RADIUS, WRIST_STEM_RADIUS, WRIST_STEM_LENGTH, 32), material);
+    wristStem.position.set(-(WRIST_STEM_LENGTH-ELBOW_WIDTH)/2, 0, 0);
     wristStem.rotation.set(0, 0, Math.PI/2);
     elbowJoint.add(wristStem);
 
     // Wrist 
-    const wrist = new THREE.Mesh(new THREE.BoxGeometry(0.4, WRIST_LENGTH, 0.4), material);
-    wrist.position.set(-0.45, -WRIST_LENGTH/2, 0);
+    const wrist = new THREE.Mesh(new THREE.BoxGeometry(WRIST_WIDTH, WRIST_LENGTH, WRIST_DEPTH), material);
+    wrist.position.set(-(WRIST_STEM_LENGTH-(ELBOW_WIDTH+WRIST_WIDTH)/2), -WRIST_LENGTH/2, 0);
     elbowJoint.add(wrist);
 
+    // Wrist joint (DOF 3)
     const wristJoint = new THREE.Object3D();
     wristJoint.position.set(0, -WRIST_LENGTH/2, 0);
     wrist.add(wristJoint);
 
-    const gripperStem = new THREE.Mesh(new THREE.CylinderGeometry(0.15, 0.15, 0.8, 32), material);
-    gripperStem.position.set(-0.2, 0, 0);
+    const gripperStem = new THREE.Mesh(new THREE.CylinderGeometry(GRIPPER_STEM_RADIUS, GRIPPER_STEM_RADIUS, GRIPPER_STEM_LENGTH, 32), material);
+    gripperStem.position.set(-(GRIPPER_STEM_LENGTH-WRIST_WIDTH)/2, 0, 0);
     gripperStem.rotation.set(0, 0, Math.PI/2);
     wristJoint.add(gripperStem);
 
-    const gripperBase = new THREE.Mesh(new THREE.BoxGeometry(GRIPPER_BASE_LENGTH, 0.2, GRIPPER_BASE_WIDTH), material);
-    gripperBase.position.set(-GRIPPER_BASE_LENGTH/2, 0.3, 0);
+    const gripperBase = new THREE.Mesh(new THREE.BoxGeometry(GRIPPER_BASE_WIDTH, GRIPPER_BASE_LENGTH, GRIPPER_BASE_DEPTH), material);
+    gripperBase.position.set(-GRIPPER_BASE_LENGTH/2, (GRIPPER_STEM_LENGTH-GRIPPER_BASE_WIDTH)/2, 0);
+    gripperBase.rotation.set(0, 0, Math.PI/2);
     gripperStem.add(gripperBase);
 
-    let openMM = 0.4;
+    const gripperStatic = new THREE.Mesh(new THREE.BoxGeometry(GRIPPERS_LENGTH, GRIPPERS_HEIGHT, GRIPPERS_WIDTH), material);
+    gripperStatic.position.set((GRIPPERS_LENGTH-GRIPPER_BASE_WIDTH)/2, -0.1, 0);
+    // gripperBase.rotation.set(0, 0, 0);
+    gripperBase.add(gripperStatic);
 
-    const gripperL = new THREE.Mesh(new THREE.BoxGeometry(GRIPPERS_LENGTH, 0.2, GRIPPERS_WIDTH), material);
-    gripperL.position.set(-GRIPPER_BASE_LENGTH/2-GRIPPERS_LENGTH/2, 0, -(GRIPPERS_WIDTH+openMM)/2);
-    gripperBase.add(gripperL);
-
-    const gripperR = new THREE.Mesh(new THREE.BoxGeometry(GRIPPERS_LENGTH, 0.2, GRIPPERS_WIDTH), material);
-    gripperR.position.set(-GRIPPER_BASE_LENGTH/2-GRIPPERS_LENGTH/2, 0, (GRIPPERS_WIDTH+openMM)/2);
-    gripperBase.add(gripperR);
+    const gripperDynamic = new THREE.Mesh(new THREE.BoxGeometry(GRIPPERS_LENGTH, GRIPPERS_HEIGHT, GRIPPERS_WIDTH), material);
+    gripperDynamic.position.set(0, 0, 0);
+    gripperStatic.add(gripperDynamic);
 
     // Animation loop for rendering
     const animate = () => {
       requestAnimationFrame(animate);
+
+      controls.update();
 
       const state = robotStateRef.current;
       if (state) {
@@ -139,6 +163,7 @@ function RobotVisualization(props: RobotVisualizationProps) {
         liftJoint.position.y = state.lift_elevation_mm / 1000; // Convert mm to meters
         elbowJoint.rotation.x = THREE.MathUtils.degToRad(state.elbow_rotation_deg);
         wristJoint.rotation.x = THREE.MathUtils.degToRad(state.wrist_rotation_deg);
+        gripperDynamic.position.y = (state.gripper_open_mm/1000+GRIPPERS_HEIGHT);
       }
 
       renderer.render(scene, camera);
